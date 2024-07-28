53484d945943033defc598b207a2ab8b9fe5ef6b
yuseiwakasa <y72147332@gmail.com>
2024-07-28 20:20:35 +0900

    条件分岐をコミット

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+// 変数宣言
+let num;
+
+// 数値の代入
+num = '30';
+
+if (num % 15 === 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
